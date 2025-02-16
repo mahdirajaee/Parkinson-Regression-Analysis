ecf2ff09ab4d927c076cc57e07a3e6e44e6255d1
Matt <mahdi.rajaee@ymail.com>
2025-02-16 23:17:48 +0100

    Remove unused imports in feature engineering module

--- parkinson_updrs_regression/feature_engineering.py
+++ parkinson_updrs_regression/feature_engineering.py
@@ -1,8 +1,5 @@
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn.preprocessing import StandardScaler
 
 def compute_feature_correlations(df):
     """
